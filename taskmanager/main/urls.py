81739d084547f9344ad5de5bdde5cbc0da7dde68
Valery Filatov <valiklfiatoff@yandex.ru>
2023-08-15 22:48:30 +0300

    Rest API completed
    Yeah!

--- taskmanager/main/urls.py
+++ taskmanager/main/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'', views.TaskViewSet)
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('about', views.about, name='about'),
-    path('create', views.create, name='create')
+    path('create', views.create, name='create'),
+    path('api', include(router.urls)),
 ]
--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
+from rest_framework import viewsets
+from .serializers import TaskSerializer
 
 
 def index(request):
@@ -36,3 +38,10 @@ def create(request):
         'error': error
     }
     return render(request, 'main/create.html', render_data)
+
+
+# API View Sets
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
